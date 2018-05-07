18400ed6797c593d566e53c1c92a13f98a5820dd
abenjamin1313 <abenjamin1313@gmail.com>
2018-05-07 19:39:51 -0400

    First angular reactive form commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+// service
+import { XyzErrorHandlerService } from './error-handler.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: XyzErrorHandlerService}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
